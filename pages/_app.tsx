378246a826a6140904e01ccb0dacdffbfabc1de8
Basharath <Basharath.in@gmail.com>
2022-07-27 15:30:36 +0530

    Updates _app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React, { useEffect, useState } from 'react';
+import AuthUser from '../types/AuthUser';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [user, setUser] = useState<AuthUser | null>(null);
+
+  const getUser = async () => {
+    try {
+      const res = await fetch('/api/users', {
+        credentials: 'include',
+      });
+      const result = await res.json();
+      if (!result.err) {
+        setUser(result.data);
+        console.log('Inside App', result.data);
+      }
+    } catch (err) {
+      console.log('User err', err);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    // <UserContext.Provider value={user}>
+    <Component {...pageProps} user={user} />
+    // </UserContext.Provider>
+  );
 }
 
 export default MyApp;
