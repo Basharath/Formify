62bb709fc7e1392e79c9dae32cb2eb934e5aa41e
Basharath <Basharath.in@gmail.com>
2022-07-30 18:54:51 +0530

    Adds charm font for logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Charm: ['Charm', 'cursive'],
+      },
+    },
   },
   plugins: [],
-}
+};
